99c1eb30857e4311750e25e768c8b61bbc4dee51
Aravind <adhaksh007@gmail.com>
2026-03-13 20:55:45 +0000

    Start screen done. Game screen canvas set.

--- Main.java
+++ Main.java
@@ -17,12 +17,15 @@ import javafx.scene.effect.Glow;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
 public class Main extends Application {
     
+    private BorderPane root = new BorderPane();
+    
     @Override
     public void start(Stage primaryStage) {
-        BorderPane root = new BorderPane();
+
         
         // Setup Menu Here 
         MenuBar menuBar = new MenuBar();
@@ -37,7 +40,7 @@ public class Main extends Application {
         Alert aboutAlert = new Alert(AlertType.INFORMATION); 
         aboutAlert.setTitle("About");
         aboutAlert.setHeaderText("Bashout");
-        aboutAlert.setContentText("Objective: Score as many points as possible by destroying bricks. \n\n" + "By: Swaraj, Aravind, Zaid, Kaveen");
+        aboutAlert.setContentText("Objective: Score as many points as possible by destroying bricks. \n" + "By: Swaraj, Aravind, Zaid, Kaveen");
         aboutItem.setOnAction((ActionEvent ev) -> {aboutAlert.showAndWait();}); 
     
         
@@ -52,41 +55,59 @@ public class Main extends Application {
         //root.setCenter(canvas);
 
         // Start Game Engine
+        showStartScreen();
         GameEngine engine = new GameEngine();
+                
+        // TODO - Set up keyboard event listeners on the Scene and pass them to GameEngine
+        engine.start(gc);
+
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setTitle("Bashout");
+        primaryStage.setScene(scene);
+        primaryStage.show();
         
+        // TODO - Add the JavaFX Animation
+    }
+    
+    private void showStartScreen(){
         // Start Screen
         VBox startScreen = new VBox(10);
         startScreen.setAlignment(Pos.CENTER);
+        startScreen.setStyle("-fx-background-color: black;");
         
-        Label title = new Label("BASHOUT");
-        title.setStyle("-fx-font-size: 40px; -fx-font-weight: bold; -fx-font-family: 'Arial'; -fx-fill: red;");
+        Label title = new Label("B A S H O U T");
+        title.setStyle("-fx-font-size: 100px; -fx-font-weight: bold; -fx-font-family: 'Impact'; -fx-text-fill: #ff073a;");
         Glow glow = new Glow(0.8);  
         title.setEffect(glow);
         
         ToggleGroup difficultyLevel = new ToggleGroup();
         RadioButton easy = new RadioButton("Easy");
         RadioButton hard = new RadioButton("Hard");
+        easy.setStyle("-fx-font-size: 25px; -fx-font-family: 'Impact'; -fx-text-fill: #dddddd;");
+        hard.setStyle("-fx-font-size: 25px; -fx-font-family: 'Impact'; -fx-text-fill: #dddddd;");
         easy.setToggleGroup(difficultyLevel);
         hard.setToggleGroup(difficultyLevel);
         easy.setSelected(true);
         
+        HBox difficultyBox = new HBox(20);
+        difficultyBox.setAlignment(Pos.CENTER);
+        difficultyBox.getChildren().addAll(easy, hard);         
+        
         Button startButton = new Button("Start Game");
-        startScreen.getChildren().addAll(title, easy, hard, startButton);
+        startButton.setStyle("-fx-font-size: 20px; -fx-font-family: 'Impact'; -fx-background-color: #ff8c00; -fx-text-fill: black; -fx-padding: 10 20 10 20;");
+        startButton.setOnAction((ActionEvent ev) -> showGameScreen());
+        
+        startScreen.getChildren().addAll(title, difficultyBox, startButton);
         startScreen.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        startScreen.setSpacing(50);
         root.setCenter(startScreen);
         
+    }
+    
+    private void showGameScreen(){
         
-        
-        
-        // TODO - Set up keyboard event listeners on the Scene and pass them to GameEngine
-        engine.start(gc);
-
-        Scene scene = new Scene(root);
-        primaryStage.setTitle("Bashout");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        
-        // TODO - Add the JavaFX Animation
+        Canvas canvas = new Canvas(800, 600);
+        root.setCenter(canvas);    
     }
 
     public static void main(String[] args) {
